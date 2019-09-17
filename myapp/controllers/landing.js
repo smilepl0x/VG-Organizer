c143832c9beebeb5798078e6f66d44dcc082ea97
smilepl0x <47682247+smilepl0x@users.noreply.github.com>
2019-09-17 17:36:50 -0500

    Shows games after submission of game

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -1,15 +1,22 @@
 const models = require('../models')
+
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
 
-exports.submit_lead = function(req, res, next) {
+exports.submit_game = function(req, res, next) {
   return models.Game.create({
     title: req.body.game_title,
     imgURL: req.body.img_url,
     description: req.body.description,
     timePlayed: req.body.time_played
-  }).then(lead => {
-    res.redirect('/');
+  }).then(game => {
+    res.redirect('/games');
+  })
+}
+
+exports.show_games = function(req, res, next) {
+  models.Game.findAll().then(games => {
+    res.render('landing', { title: 'Express', games: games });
   })
 }
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 /* GET home page. */
 router.get('/', landing.get_landing);
-router.post('/', landing.submit_lead);
+router.post('/', landing.submit_game);
+router.get('/games', landing.show_games);
 module.exports = router;
